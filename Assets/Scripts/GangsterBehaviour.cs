16581870b0d6437551450312a8b5eb29553617d9
GuillaumeCharvet <guicharvet@orange.fr>
2022-05-14 11:48:31 +0200

    Changements caméra

---
+++ Assets/Scripts/GangsterBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GangsterBehaviour : MonoBehaviour
+{
+    public int gang_appartenance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
